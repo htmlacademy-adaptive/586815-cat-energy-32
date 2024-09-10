e95e929937e90bdb0e80716ed06c68c55caba5ef
TopolskiiDaniil <cool.topolskiy@gmail.com>
2024-09-10 15:53:31 +0300

    мобильное меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+const checkbox = document.getElementById('menu-toggle');
+const navigation = document.querySelector('.navigation');
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    navigation.classList.add('navigation--show');
+  } else {
+    navigation.classList.remove('navigation--show');
+  }
+});

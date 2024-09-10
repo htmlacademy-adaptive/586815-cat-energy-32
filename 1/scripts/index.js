ab3926d6eba20c12b34ac52f3f5f2fb5b1140d17
TopolskiiDaniil <TopolskiiDaniil@users.noreply.github.com>
2024-09-10 12:56:19 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/586815-cat-energy-32@c55ad7d37b3a2da87d1c27639ef10f38564e946b 🚀

---
+++ 1/scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.getElementById("menu-toggle"),n=document.querySelector(".navigation");e.addEventListener("change",()=>{e.checked?n.classList.add("navigation--show"):n.classList.remove("navigation--show")});

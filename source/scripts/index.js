ac173acdeca933f1b689b238dce71d82de57b583
TopolskiiDaniil <cool.topolskiy@gmail.com>
2024-09-16 16:00:14 +0300

    Адаптивные декоративные элементы (#12)
    * мобильное меню
    
    * вернул временные пустые страницы
    
    * адаптивные декоративные элементы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
-/* в этот файл добавляет скрипты*/
+const checkbox = document.getElementById('menu-toggle');
+const navigation = document.querySelector('.navigation');
+
+navigation.classList.remove('navigation--no-js');
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    navigation.classList.add('navigation--show');
+  } else {
+    navigation.classList.remove('navigation--show');
+  }
+});

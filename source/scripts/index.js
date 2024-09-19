e416e20d70d73c521cf66bb86de9a54c3bd4a9f9
TopolskiiDaniil <cool.topolskiy@gmail.com>
2024-09-19 17:29:51 +0300

    Производительность вёрстки + стилизация подписки (#21)
    * адаптивные декоративные элементы8
    
    * производительность вёрстки + стилизация подписки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -10,3 +10,26 @@ checkbox.addEventListener('change', () => {
     navigation.classList.remove('navigation--show');
   }
 });
+
+const form = document.querySelector('.subscription__form');
+const emailInput = document.querySelector('.subscription__input');
+
+if (form && emailInput) {
+  form.addEventListener('submit', (event) => {
+    if (!emailInput.checkValidity()) {
+      emailInput.classList.add('subscription__input--invalid');
+      event.preventDefault(); // Блокируем отправку формы
+    } else {
+      emailInput.classList.remove('subscription__input--invalid'); // Убираем класс при корректном вводе
+    }
+  });
+
+  // Проверка валидности в реальном времени
+  emailInput.addEventListener('input', () => {
+    if (emailInput.checkValidity()) {
+      emailInput.classList.remove('subscription__input--invalid');
+    } else {
+      emailInput.classList.add('subscription__input--invalid');
+    }
+  });
+}

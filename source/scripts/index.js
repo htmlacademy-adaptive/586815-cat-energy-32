43f5eb465a22755f2b60f0b8cf93a33b0ccbdb5a
TopolskiiDaniil <cool.topolskiy@gmail.com>
2024-09-20 18:45:40 +0300

    Настройка слайдера (#22)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -11,25 +11,54 @@ checkbox.addEventListener('change', () => {
   }
 });
 
-const form = document.querySelector('.subscription__form');
-const emailInput = document.querySelector('.subscription__input');
+const button = document.querySelector('.example__button');
+const slider = document.querySelector('.example__slider');
 
-if (form && emailInput) {
-  form.addEventListener('submit', (event) => {
-    if (!emailInput.checkValidity()) {
-      emailInput.classList.add('subscription__input--invalid');
-      event.preventDefault(); // Блокируем отправку формы
-    } else {
-      emailInput.classList.remove('subscription__input--invalid'); // Убираем класс при корректном вводе
-    }
-  });
+button.addEventListener('mousedown', (event) => {
+  event.preventDefault();
 
-  // Проверка валидности в реальном времени
-  emailInput.addEventListener('input', () => {
-    if (emailInput.checkValidity()) {
-      emailInput.classList.remove('subscription__input--invalid');
-    } else {
-      emailInput.classList.add('subscription__input--invalid');
+  const shiftX = event.clientX - button.getBoundingClientRect().left;
+  const sliderRect = slider.getBoundingClientRect();
+
+  function moveAt(pageX) {
+    let newLeft = pageX - shiftX - sliderRect.left;
+
+    // Ограничиваем положение кнопки по оси X
+    if (newLeft < 0) {
+      newLeft = 0;
     }
-  });
-}
+    if (newLeft > sliderRect.width - button.offsetWidth) {
+      newLeft = sliderRect.width - button.offsetWidth;
+    }
+
+    button.style.left = `${newLeft }px`;
+
+    // Центрируем линию под кнопкой
+    const linePosition = newLeft + button.offsetWidth / 2;
+    slider.style.setProperty('--line-left', `${linePosition}px`);
+
+    // Меняем ширину колонок в зависимости от положения кнопки
+    const leftColumnWidth = (newLeft + button.offsetWidth / 2) / sliderRect.width * 100;
+    const rightColumnWidth = 100 - leftColumnWidth;
+
+    slider.style.gridTemplateColumns = `${leftColumnWidth}% ${rightColumnWidth}%`;
+  }
+
+  function onMouseMove(mouseMoveEvent) {
+    moveAt(mouseMoveEvent.pageX);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  document.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.onmouseup = null;
+  };
+});
+
+button.ondragstart = function() {
+  return false;
+};
+
+// Установка начального положения кнопки
+button.style.left = 'calc(50% - 20px)'; // По центру
